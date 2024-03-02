7c15aec0808d2e1355a7066783ad5c442a87aa17
Alexander Shepelenko <99736869+AlexAllGold@users.noreply.github.com>
2024-03-02 16:24:18 +0200

    feat: added wrapperController

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,21 +1,27 @@
 import express from 'express';
 import { commentController } from '../controllers/commentController';
-import { userController } from '../controllers/userController'
-import {validationMiddleware} from "../middlewares/validation.middleware";
-import {CreateUserDto} from "../dtos/createUser.dto";
-import {UpdateUserDto} from "../dtos/updateUser.dto";
+import { userController } from '../controllers/userController';
+import { validationMiddleware } from '../middlewares/validation.middleware';
+import { CreateUserDto } from '../dtos/createUser.dto';
+import { UpdateUserDto } from '../dtos/updateUser.dto';
+import { wrapperController } from '../utils/wrapperController';
+import { CreateCommentDto } from '../dtos/createComment.dto';
+import { UpdateCommentDto } from '../dtos/updateComment.dto';
 
 export const router = express.Router();
 
+router.get('/user', wrapperController(userController.getAll));
+router.post('/user', validationMiddleware<CreateUserDto>(CreateUserDto), wrapperController(userController.create));
+router.get('/user/:userId', wrapperController(userController.getOne));
+router.put('/user/:userId', validationMiddleware<UpdateUserDto>(UpdateUserDto), wrapperController(userController.update));
+router.delete('/user/:userId', wrapperController(userController.remove));
 
-router.get('/user', userController.getAll);
-router.post('/user', validationMiddleware<CreateUserDto>(CreateUserDto), userController.create);
-router.get('/user/:userId', userController.getOne);
-router.put('/user/:userId', validationMiddleware<UpdateUserDto>(UpdateUserDto), userController.update);
-router.delete('/user/:userId', userController.remove);
-
-router.get('/comments', commentController.getAllByUserId);
-router.post('/comments', commentController.create);
-router.get('/comments/:commentId', commentController.getOne);
-router.put('/comments/:commentsId', commentController.update);
-router.delete('/comments/:commentsId', commentController.remove);
+router.get('/user/:userId/comments', wrapperController(commentController.getAllByUserId));
+router.post('/user/:userId/comments', validationMiddleware<CreateCommentDto>(CreateCommentDto), wrapperController(commentController.create));
+router.get('/user/:userId/comments/:commentId', wrapperController(commentController.getOne));
+router.put(
+  '/user/:userId/comments/:commentId',
+  validationMiddleware<UpdateCommentDto>(UpdateCommentDto),
+  wrapperController(commentController.update),
+);
+router.delete('/user/:userId/comments/:commentId', wrapperController(commentController.remove));
---
+++ src/utils/wrapperController.ts
@@ -0,0 +1,7 @@
+import { type NextFunction, type Request, type Response } from 'express';
+
+type ControllerFnType = (req: Request, res: Response, next: NextFunction) => Promise<void>;
+
+export const wrapperController = (controller: ControllerFnType): ControllerFnType => async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    await controller(req, res, next).catch(next);
+  };
