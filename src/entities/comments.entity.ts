43c92b919f68183f829d68a7483de55ee8de5085
Alexander Shepelenko <99736869+AlexAllGold@users.noreply.github.com>
2024-03-02 16:17:22 +0200

    feat: added relation in databases

--- src/entities/comments.entity.ts
+++ src/entities/comments.entity.ts
@@ -1,28 +1,31 @@
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { UsersEntity } from './users.entity';
 
 @Entity({ name: 'comments' })
 export class CommentsEntity {
   @PrimaryGeneratedColumn()
-    // @ts-expect-error
-    id: number;
+  id!: number;
 
   @Column('int', { nullable: false })
-    // @ts-expect-error
-    userId: number;
+  userId!: number;
 
   @Column('int', { nullable: true })
-    parentCommentId?: number;
+  parentCommentId?: number;
 
   @Column('varchar', { length: 1024, nullable: false })
-    // @ts-expect-error
-    commentText: string;
+  commentText!: string;
 
   @Column('timestamp')
-    // @ts-expect-error
-    commentDate: Date;
+  commentDate!: Date;
 
   @ManyToOne(() => UsersEntity, (user: UsersEntity) => user.comments)
-    // @ts-expect-error
-    user: UsersEntity;
+  user!: UsersEntity;
+
+  @OneToMany(() => CommentsEntity, (comment) => comment.parentComment, { cascade: true })
+  @JoinColumn({ name: 'parentCommentId' })
+  comments!: CommentsEntity[];
+
+  @ManyToOne(() => CommentsEntity)
+  @JoinColumn({ name: 'parentCommentId' })
+  parentComment!: CommentsEntity;
 }
