a2304b2284296015381affa936d682cc6c92f116
Alexander Shepelenko <99736869+AlexAllGold@users.noreply.github.com>
2024-03-02 16:07:20 +0200

    feat: added a function for tree traversal

--- src/controllers/commentController.ts
+++ src/controllers/commentController.ts
@@ -1,37 +1,33 @@
 import { type Request, type Response } from 'express';
 import { CommentsEntity } from '../entities/comments.entity';
 import { commentService } from '../services/commentService';
-import { CreateCommentDto } from '../dtos/createComment.dto';
-import { UpdateCommentDto } from '../dtos/updateComment.dto';
+import { PaginationDto } from "../dtos/pagination.dto";
 
 class CommentController {
   async getAllByUserId(req: Request, res: Response): Promise<void> {
-    // @ts-ignore
-    const comment: CommentsEntity[] = await commentService.getAllByUserId(Number(req.params.userId));
+    const comment: CommentsEntity[] = await commentService.getAllComments(Number(req.params.userId));
     res.status(200).json(comment);
   }
 
   async getOne(req: Request, res: Response): Promise<void> {
-    // @ts-ignore
-    const comment: CommentsEntity = await commentService.getOne(Number(req.params.userId), Number(req.params.commentId));
+    const { page, limit } = new PaginationDto(req.query)
+    const comment: CommentsEntity[] = await commentService.getOneTree(Number(req.params.userId), Number(req.params.commentId), {page, limit} );
     res.status(200).json(comment);
   }
 
   async create(req: Request, res: Response): Promise<void> {
-    // @ts-ignore
-    const comment: CommentsEntity = await commentService.create(req.params, req.body as CreateCommentDto);
+    const comment: CommentsEntity = await commentService.create(req.params, req.body);
     res.status(201).json(comment);
   }
 
   async update(req: Request, res: Response): Promise<void> {
-    // @ts-ignore
-    const comment: CommentsEntity = await commentService.update(req.params, req.body as UpdateCommentDto);
-    res.status(204).json(comment);
+    const comment: CommentsEntity = await commentService.update(req.params, req.body);
+    res.status(202).json(comment);
   }
 
   async remove(req: Request, res: Response): Promise<void> {
     await commentService.remove(Number(req.params.userId), Number(req.params.commentId));
-    res.status(400).end().json('deleted');
+    res.status(204).end();
   }
 }
 
--- src/services/commentService.ts
+++ src/services/commentService.ts
@@ -1,59 +1,123 @@
-import { DeepPartial, type Repository } from "typeorm";
+import { Repository } from 'typeorm';
+import { Database } from '../config/database';
 // import { BadRequestException } from '../utils/exceptions/BadRequestException';
 import { CreateCommentDto } from '../dtos/createComment.dto';
-import { CommentsEntity } from '../entities/comments.entity';
-import { Database } from '../config/database';
 import { UpdateCommentDto } from '../dtos/updateComment.dto';
+import { PaginationDto } from '../dtos/pagination.dto'
+import { CommentsEntity } from '../entities/comments.entity';
 
 class CommentService {
   commentRepository: Repository<CommentsEntity>;
+
+  //TODO: изменить page на 25 после полного теста!
 
   constructor() {
     this.commentRepository = Database.dataSource.getRepository(CommentsEntity);
   }
 
-  async getAllByUserId(userId: number): Promise<CommentsEntity[]> {
-    return this.commentRepository.findBy({ userId });
+  async getAllComments(userId: number): Promise<CommentsEntity[]> {
+    return this.commentRepository.createQueryBuilder('comments')
+      .where('comments.userId = :userId', { userId })
+      .andWhere('comments.parentCommentId IS NULL')
+      .getMany();
   }
 
-  async getOne(userId: number, id: number): Promise<CommentsEntity> {
-    console.log( userId, id);
-
-    const comment: CommentsEntity | null = await this.commentRepository.findOneBy({ userId, id });
+  async getOneTree(userId: number, id: number, { page, limit } : PaginationDto): Promise<CommentsEntity[]> {
+    const comment: CommentsEntity | null = await this.commentRepository.findOne({
+      where: {
+        userId,
+        id,
+      },
+      relations: {
+        comments: true,
+        parentComment: true
+      },
+    });
     if (comment) {
-      return comment;
+      console.log(page, limit)
+      const totalLimit = page! * limit!;
+      return comment.comments = await this.getCommentTree([], comment, totalLimit);
     }
     // throw new BadRequestException('Can not find this comment!');
     throw new Error('Can not find this comment!');
   }
 
-  async create(params: Record<string, string>, dto: CreateCommentDto): Promise<DeepPartial<CommentsEntity>[]> {
+  async create(params: Record<string, string>, dto: CreateCommentDto): Promise<CommentsEntity> {
     this.compareIds(Number(params.userId), Number(dto.userId));
     // @ts-ignore
     return this.commentRepository.save(dto);
   }
 
   async update(params: Record<string, string>, dto: UpdateCommentDto): Promise<CommentsEntity> {
-    this.compareIds(Number(params.id), dto.commentId);
-    this.compareIds(Number(params.commentId), dto.commentId);
-    const comment: CommentsEntity = await this.getOne(dto.userId, dto.commentId);
+    this.compareIds(Number(params.userId), dto.userId);
+    this.compareIds(Number(params.commentId), dto.id);
+    const comment: CommentsEntity = await this.getOne(dto.userId, dto.id);
     // @ts-ignore
     this.commentRepository.merge(comment, dto);
-    return await this.commentRepository.save(comment);
+    return this.commentRepository.save(comment);
   }
 
   async remove(userId: number, id: number): Promise<void> {
-    console.log( userId, id);
     await this.getOne(userId, id);
     await this.commentRepository.delete(id);
   }
 
   private compareIds(id: number, dtoId: number): void {
+    console.log(id, dtoId);
     if (id !== Number(dtoId)) {
       // throw new BadRequestException('Id Comment does not match');
       throw new Error('Id Comment does not match');
     }
   }
+
+  private async getOne(userId: number, id: number): Promise<CommentsEntity> {
+    const comment: CommentsEntity | null = await this.commentRepository.findOne({
+      where: {
+        userId,
+        id,
+      }
+    });
+    if (!comment) {
+      throw new Error('Can not find this comment!');
+
+    }
+    return comment;
+
+    // throw new BadRequestException('Can not find this comment!');
+  }
+
+  private async getCommentTree(result : CommentsEntity[],parentComment: CommentsEntity, totalLimit: number): Promise<CommentsEntity[]> {
+    if (!parentComment || !parentComment.id || totalLimit! <= 0) {
+      return [];
+    }
+    const comment = await this.commentRepository.findOne({
+      where: {
+        id: parentComment.id,
+      },
+      relations: ['comments'],
+    });
+
+    if (!comment) {
+      return [];
+    }
+    const sliceIndex =  totalLimit!;
+
+      for (let i = 0; i <= totalLimit!; i++) {
+            const childComment: CommentsEntity = comment.comments[i];
+        if (comment.comments && comment.comments.length !== undefined && childComment) {
+          --totalLimit;
+            result.push(childComment);
+              if (true!) {
+              await this.getCommentTree(result, childComment, totalLimit);
+            }
+          }
+        }
+
+    comment.comments = result.slice(0, sliceIndex);
+
+    return [comment];
+  }
+
 }
 
 export const commentService: CommentService = new CommentService();
